c39c87ced29f31aa9c114d59f11549ee51bbd799
Francisco Casillas <franky.casillas09@gmail.com>
2023-08-04 16:41:11 -0700

    "messing with css styling"

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ let getBev = () => {
         document.getElementById("user-inp").value = "";
         console.log(data);
         console.log(data.drinks[0]);
-        let myDrink = data.drinks[0];
-        console.log(myDrink.strDrink);
-        console.log(myDrink.strDrinkThumb);
-        console.log(myDrink.strInstructions);
+        let myBev = data.drinks[0];
+        console.log(myBev.strDrink);
+        console.log(myBev.strDrinkThumb);
+        console.log(myBev.strInstructions);
         let count = 1;
         let ingredients = [];
-        for (let i in myDrink) {
+        for (let i in myBev) {
           let ingredient = "";
           let measure = "";
-          if (i.startsWith("strIngredient") && myDrink[i]) {
-            ingredient = myDrink[i];
-            if (myDrink[`strMeasure` + count]) {
-              measure = myDrink[`strMeasure` + count];
+          if (i.startsWith("strIngredient") && myBev[i]) {
+            ingredient = myBev[i];
+            if (myBev[`strMeasure` + count]) {
+              measure = myBev[`strMeasure` + count];
             } else {
               measure = "";
             }
@@ -34,12 +34,12 @@ let getBev = () => {
         }
         console.log(ingredients);
         result.innerHTML = `
-      <img src=${myDrink.strDrinkThumb}>
-      <h2>${myDrink.strDrink}</h2>
+      <img src=${myBev.strDrinkThumb}>
+      <h2>${myBev.strDrink}</h2>
       <h3>Ingredients:</h3>
       <ul class="ingredients"></ul>
       <h3>Instructions:</h3>
-      <p>${myDrink.strInstructions}</p>
+      <p>${myBev.strInstructions}</p>
       `;
         let ingredientsCon = document.querySelector(".ingredients");
         ingredients.forEach((item) => {
