02903ed939969542ae96b4c49fcb5473330aa504
Francisco Casillas <franky.casillas09@gmail.com>
2023-08-04 14:14:39 -0700

    "js changes and html"

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-let url = "www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
-let result = document.getElementById("result");
-let searchBtn = document.getElementById("search-btn");
+const searchBar = document.body.querySelector.querySelector("#search-bar");
+
+const getCocktailNames = async () => {
+  const cocktailNamesApiURL =
+    "www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+  try {
+    const response = await fetch(cocktailNamesApiURL);
+    const data = await response.json();
+    const names = data.names;
+    return names;
+  } catch (error) {
+    console.log(error);
+    alert("Invalid, try again later");
+  }
+};
+
+cons;
