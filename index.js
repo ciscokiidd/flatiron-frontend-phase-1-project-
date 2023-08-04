9e88c9b480ae3addc20ee0e49d90e3453e4f1bc4
Francisco Casillas <franky.casillas09@gmail.com>
2023-08-04 14:31:29 -0700

    "added js, html, and css changes"

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
-const searchBar = document.body.querySelector.querySelector("#search-bar");
-
-const getCocktailNames = async () => {
-  const cocktailNamesApiURL =
-    "www.thecocktaildb.com/api/json/v1/1/search.php?s=";
-
-  try {
-    const response = await fetch(cocktailNamesApiURL);
-    const data = await response.json();
-    const names = data.names;
-    return names;
-  } catch (error) {
-    console.log(error);
-    alert("Invalid, try again later");
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let url = "https://thecocktaildb.com/api/json/v1/1/search.php?s=";
+let getInfo = () => {
+  let userInp = document.getElementById("user-inp").value;
+  if (userInp.length == 0) {
+    result.innerHTML = `<h3 class="msg">The input field cannot be empty</h3>`;
+  } else {
+    fetch(url + userInp)
+      .then((response) => response.json())
+      .then((data) => {
+        document.getElementById("user-inp").value = "";
+        console.log(data);
+        console.log(data.drinks[0]);
+        let myDrink = data.drinks[0];
+        console.log(myDrink.strDrink);
+        console.log(myDrink.strDrinkThumb);
+        console.log(myDrink.strInstructions);
+        let count = 1;
+        let ingredients = [];
+        for (let i in myDrink) {
+          let ingredient = "";
+          let measure = "";
+          if (i.startsWith("strIngredient") && myDrink[i]) {
+            ingredient = myDrink[i];
+            if (myDrink[`strMeasure` + count]) {
+              measure = myDrink[`strMeasure` + count];
+            } else {
+              measure = "";
+            }
+            count += 1;
+            ingredients.push(`${measure} ${ingredient}`);
+          }
+        }
+        console.log(ingredients);
+        result.innerHTML = `
+      <img src=${myDrink.strDrinkThumb}>
+      <h2>${myDrink.strDrink}</h2>
+      <h3>Ingredients:</h3>
+      <ul class="ingredients"></ul>
+      <h3>Instructions:</h3>
+      <p>${myDrink.strInstructions}</p>
+      `;
+        let ingredientsCon = document.querySelector(".ingredients");
+        ingredients.forEach((item) => {
+          let listItem = document.createElement("li");
+          listItem.innerText = item;
+          ingredientsCon.appendChild(listItem);
+        });
+      })
+      .catch(() => {
+        result.innerHTML = `<h3 class="msg">Please enter a valid input</h3>`;
+      });
   }
 };
-
-cons;
+window.addEventListener("load", getInfo);
+searchBtn.addEventListener("click", getInfo);
